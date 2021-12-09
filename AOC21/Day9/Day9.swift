a4cc1706387d6496dbf120623f552782e0010079
Micah Wilson <wilsonmi94@gmail.com>
2021-12-08 22:57:20 -0700

    Minor improvements

--- AOC21/Day9/Day9.swift
+++ AOC21/Day9/Day9.swift
@@ -8,10 +8,8 @@
 import Foundation
 
 struct Day9: Day {
-    func part1() -> String {
-        let input = input.columns.map { $0.ints }
-        
-        var risk = 0
+    func basins(input: [[Int]]) -> [Point] {
+        var basins = [Point]()
         for col in input.indices {
             for row in input[col].indices {
                 let above = col > 0 ? input[col - 1][row] : 9
@@ -21,38 +19,34 @@ struct Day9: Day {
                 let value = input[col][row]
                 
                 if value < above && value < below && value < left && value < right {
-                    risk += value + 1
+                    basins.append(.init(x: col, y: row))
                 }
             }
         }
+        
+        return basins
+    }
+    
+    func part1() -> String {
+        let input = input.columns.map { $0.ints }
+        let basins = basins(input: input)
+        
+        let risk = basins.reduce(into: 0) { partialResult, point in
+            partialResult += input[point.x][point.y] + 1
+        }
+        
         return "\(risk)"
     }
     
     func part2() -> String {
         let input = input.columns.map { $0.ints }
-        
-        var risk = 0
-        var basins = [(col: Int, row: Int)]()
-        for col in input.indices {
-            for row in input[col].indices {
-                let above: Int = col > 0 ? input[col - 1][row] : .max
-                let below: Int = col < input.count - 1 ? input[col + 1][row] : .max
-                let left: Int = row > 0 ? input[col][row - 1] : .max
-                let right: Int = row < input[col].count - 1 ? input[col][row + 1] : .max
-                let value = input[col][row]
-                
-                if value < above && value < below && value < left && value < right {
-                    risk += value + 1
-                    basins.append((col: col, row: row))
-                }
-            }
-        }
+        let basins = basins(input: input)
         
         let basinSizes = basins.map { basin -> Int in
             var size = 0
             var checkedInput = input
-            var col = basin.col
-            var row = basin.row
+            var col = basin.x
+            var row = basin.y
             
             var checks = Set([Point(x: col, y: row)])
             while !checks.isEmpty {
